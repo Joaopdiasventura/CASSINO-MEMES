eff58f3f55f3c90acb69415db80d908c0040f581
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-02-07 05:34:22 -0300

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace cassino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("SimSun-ExtB", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(343, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "INICIAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("SimSun-ExtB", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(21, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PARAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("SimSun-ExtB", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(178, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "PARAR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::cassino.Properties.Resources.vini;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(169, 109);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("SimSun-ExtB", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Yellow;
+            this.button4.Location = new System.Drawing.Point(352, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "PARAR";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::cassino.Properties.Resources.gael;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(343, 109);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 96);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("SimSun-ExtB", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Yellow;
+            this.button5.Location = new System.Drawing.Point(521, 211);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 36);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "PARAR";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::cassino.Properties.Resources.monday;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(512, 109);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 96);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Font = new System.Drawing.Font("SimSun-ExtB", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Yellow;
+            this.button6.Location = new System.Drawing.Point(697, 211);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 36);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "PARAR";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = global::cassino.Properties.Resources.medo;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(688, 109);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 96);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("SimSun-ExtB", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(174, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "BEM VINDO AO CASSINO DOS MEMES";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::cassino.Properties.Resources.piter;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::cassino.Properties.Resources.novo_cassino;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CASSINO DOS MEMES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cassino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox5.BackgroundImageLayout = ImageLayout.Stretch;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (timer1.Enabled == false && timer2.Enabled == false && timer3.Enabled == false && timer4.Enabled == false && timer5.Enabled == false)
+            {
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+                timer3.Enabled = true;
+                timer4.Enabled = true;
+                timer5.Enabled = true;
+
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+            int x = gera.Next(0, 10);
+
+            if (x == 1)
+            {
+                pictureBox1.Image = Properties.Resources.indoali;
+
+            }
+
+            else if (x == 2)
+            {
+                pictureBox1.Image = Properties.Resources.elemorango;
+            }
+
+            else if (x == 3)
+            {
+                pictureBox1.Image = Properties.Resources.vini;
+            }
+
+            else if (x == 4)
+            {
+                pictureBox1.Image = Properties.Resources.chupetao;
+            }
+
+            else if (x == 5)
+            {
+                pictureBox1.Image = Properties.Resources.piter;
+            }
+
+            else if (x == 6)
+            {
+                pictureBox1.Image = Properties.Resources.monday;
+            }
+
+            else if (x == 7)
+            {
+                pictureBox1.Image = Properties.Resources.miles;
+            }
+
+            else if (x == 8)
+            {
+                pictureBox1.Image = Properties.Resources.sigma;
+            }
+
+            else if (x == 9)
+            {
+                pictureBox1.Image = Properties.Resources.medo;
+            }
+
+            else if (x == 10)
+            {
+                pictureBox1.Image = Properties.Resources.gael;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+            int x = gera.Next(0, 10);
+
+            if (x == 1)
+            {
+                pictureBox2.Image = Properties.Resources.indoali;
+
+            }
+
+            else if (x == 2)
+            {
+                pictureBox2.Image = Properties.Resources.elemorango;
+            }
+
+            else if (x == 3)
+            {
+                pictureBox2.Image = Properties.Resources.vini;
+            }
+
+            else if (x == 4)
+            {
+                pictureBox2.Image = Properties.Resources.chupetao;
+            }
+
+            else if (x == 5)
+            {
+                pictureBox2.Image = Properties.Resources.piter;
+            }
+
+            else if (x == 6)
+            {
+                pictureBox2.Image = Properties.Resources.monday;
+            }
+
+            else if (x == 7)
+            {
+                pictureBox2.Image = Properties.Resources.miles;
+            }
+
+            else if (x == 8)
+            {
+                pictureBox2.Image = Properties.Resources.sigma;
+            }
+
+            else if (x == 9)
+            {
+                pictureBox2.Image = Properties.Resources.medo;
+            }
+
+            else if (x == 10)
+            {
+                pictureBox2.Image = Properties.Resources.gael;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+            int x = gera.Next(0, 10);
+
+            if (x == 1)
+            {
+                pictureBox3.Image = Properties.Resources.indoali;
+
+            }
+
+            else if (x == 2)
+            {
+                pictureBox3.Image = Properties.Resources.elemorango;
+            }
+
+            else if (x == 3)
+            {
+                pictureBox3.Image = Properties.Resources.vini;
+            }
+
+            else if (x == 4)
+            {
+                pictureBox3.Image = Properties.Resources.chupetao;
+            }
+
+            else if (x == 5)
+            {
+                pictureBox3.Image = Properties.Resources.piter;
+            }
+
+            else if (x == 6)
+            {
+                pictureBox3.Image = Properties.Resources.monday;
+            }
+
+            else if (x == 7)
+            {
+                pictureBox3.Image = Properties.Resources.miles;
+            }
+
+            else if (x == 8)
+            {
+                pictureBox3.Image = Properties.Resources.sigma;
+            }
+
+            else if (x == 9)
+            {
+                pictureBox3.Image = Properties.Resources.medo;
+            }
+
+            else if (x == 10)
+            {
+                pictureBox3.Image = Properties.Resources.gael;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+            int x = gera.Next(0, 10);
+
+            if (x == 1)
+            {
+                pictureBox4.Image = Properties.Resources.indoali;
+
+            }
+
+            else if (x == 2)
+            {
+                pictureBox4.Image = Properties.Resources.elemorango;
+            }
+
+            else if (x == 3)
+            {
+                pictureBox4.Image = Properties.Resources.vini;
+            }
+
+            else if (x == 4)
+            {
+                pictureBox4.Image = Properties.Resources.chupetao;
+            }
+
+            else if (x == 5)
+            {
+                pictureBox4.Image = Properties.Resources.piter;
+            }
+
+            else if (x == 6)
+            {
+                pictureBox4.Image = Properties.Resources.monday;
+            }
+
+            else if (x == 7)
+            {
+                pictureBox4.Image = Properties.Resources.miles;
+            }
+
+            else if (x == 8)
+            {
+                pictureBox4.Image = Properties.Resources.sigma;
+            }
+
+            else if (x == 9)
+            {
+                pictureBox4.Image = Properties.Resources.medo;
+            }
+
+            else if (x == 10)
+            {
+                pictureBox4.Image = Properties.Resources.gael;
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+            int x = gera.Next(0, 10);
+
+            if (x == 1)
+            {
+                pictureBox5.Image = Properties.Resources.indoali;
+
+            }
+
+            else if (x == 2)
+            {
+                pictureBox5.Image = Properties.Resources.elemorango;
+            }
+
+            else if (x == 3)
+            {
+                pictureBox5.Image = Properties.Resources.vini;
+            }
+
+            else if (x == 4)
+            {
+                pictureBox5.Image = Properties.Resources.chupetao;
+            }
+
+            else if (x == 5)
+            {
+                pictureBox5.Image = Properties.Resources.piter;
+            }
+
+            else if (x == 6)
+            {
+                pictureBox5.Image = Properties.Resources.monday;
+            }
+
+            else if (x == 7)
+            {
+                pictureBox5.Image = Properties.Resources.miles;
+            }
+
+            else if (x == 8)
+            {
+                pictureBox5.Image = Properties.Resources.sigma;
+            }
+
+            else if (x == 9)
+            {
+                pictureBox5.Image = Properties.Resources.medo;
+            }
+
+            else if (x == 10)
+            {
+                pictureBox5.Image = Properties.Resources.gael;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer4.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer5.Enabled = false;
+        }
+    }
+}
